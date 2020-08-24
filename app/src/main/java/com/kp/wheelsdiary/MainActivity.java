1c4f4183bd188c4eee930108712d276d9919a6bc
Kostadin Penev <kostadin.penev@nevexis.com>
2020-08-24 22:49:26 +0300

    fokin el

--- app/src/main/java/com/kp/wheelsdiary/MainActivity.java
+++ app/src/main/java/com/kp/wheelsdiary/MainActivity.java
@@ -1,20 +1,35 @@
 package com.kp.wheelsdiary;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 import com.kp.wheelsdiary.ui.login.LoginActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    DrawerLayout drawerLayout;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +49,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 123) {
             if (resultCode == Activity.RESULT_OK) {
                 String result = data.getStringExtra("result");
-                View viewById = findViewById(R.id.greeting);
-                TextView greeting = (TextView) viewById;
-                greeting.setText(String.format("Hello %s", result));
+//                View viewById = findViewById(R.id);
+//                TextView greeting = (TextView) viewById;
+//                greeting.setText(String.format("Hello %s", result));
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 // Not logged in
@@ -55,5 +69,73 @@ public class MainActivity extends AppCompatActivity {
         int asd = R.menu.menu;
         inflater.inflate(asd, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if(drawerLayout != null)
+                    drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupNavigationView(){
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    private void setupCollapsingToolbarLayout(){
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        if(collapsingToolbarLayout != null){
+            collapsingToolbarLayout.setTitle(toolbar.getTitle());
+            //collapsingToolbarLayout.setCollapsedTitleTextColor(0xED1C24);
+            //collapsingToolbarLayout.setExpandedTitleColor(0xED1C24);
+        }
+    }
+
+    private void setupTablayout(){
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        if(tabLayout == null)
+            return;
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+    }
+
+    private void setupFab(){
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        if(fab != null)
+            fab.setOnClickListener(this);
+    }
+    private void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null)
+            setSupportActionBar(toolbar);
+
+        // Show menu icon
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.fab){
+
+            Snackbar
+                    .make(findViewById(R.id.coordinatorLayout), "This is Snackbar", Snackbar.LENGTH_LONG)
+                    .setAction("Action", this)
+                    .show(); // Don’t forget to show!
+        }
     }
 }
